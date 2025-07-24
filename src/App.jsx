95002fb00176620534c9fc56fca9c49cfbf3e7cf
shivang ghildiyal <shivang.ghildiyal@gmail.com>
2025-07-24 17:50:45 +0530

    Tect commit in feature

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Test Application</h1>
+      <h1>Test Application Checkin</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
